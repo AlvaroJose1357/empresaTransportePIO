e02d8a6146d13ac5b2777bb4186ab590290e38e2
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-11-13 22:07:25 -0500

    description proyect and add cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
