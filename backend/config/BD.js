71ab17444cdc4a2c1b7feb377efdb6f8a85155d6
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-11-14 23:03:57 -0500

    conection postgres

--- backend/config/BD.js
+++ backend/config/BD.js
@@ -1,10 +1,12 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "./entorno.js";
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
