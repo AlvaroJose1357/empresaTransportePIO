b9f6f46efa3890e2f0eecad21527ccf4f8cd3331
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-11-19 21:43:05 -0500

    feat(token2)

--- backend/controllers/controllerAuth.js
+++ backend/controllers/controllerAuth.js
@@ -65,5 +65,6 @@ export const login = async (req, res) => {
   }
 };
 export const logout = (req, res) => {
+  res.clearCookie("token");
   res.status(200).json({ message: "Usuario deslogueado" });
 };
