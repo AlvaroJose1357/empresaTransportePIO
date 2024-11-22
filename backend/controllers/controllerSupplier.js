89ef42c9fa5881b47bdde9cba2a727f6b3eecfd6
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-11-21 22:02:35 -0500

    feat( supplier ✅)

--- backend/controllers/controllerSupplier.js
+++ backend/controllers/controllerSupplier.js
@@ -1,19 +1,26 @@
-import {pool} from "../config/BD.js"
+import { pool } from "../config/BD.js";
 // el usuario da en un campo text todo el json formado
 export const supplierOrder = async (req, res) => {
-    try{
-        const {jsonData} = req.body;
-        if (!jsonData){
-        return res.status(400).json({message: "No se pudo cargar el archivo JSON"})
-        }
-        const parsedData = JSON.parse(jsonData)
-        console.log(parsedData)
-        const newData = await pool.query("INSERT INTO nombre_tabla (columna1, columna2 VALUES ($1, $2)", [valor1, valor2])
-        res.status(200).json({message: "datos procesados", data: newData})
-    }catch(error){
-        res.status(500).json({message: "error al cargar el archivo en el servidor", error});
+  const { origen, destino, detalles, nombredestinatario } = req.body;
+  try {
+    if (!origen || !destino || !detalles || !nombredestinatario) {
+      return res
+        .status(400)
+        .json({ message: "No se pudo cargar el archivo JSON" });
     }
-}
+    const newData = await pool.query(
+      "INSERT INTO orden ( origen, destino, detalles, nombredestinatario) VALUES ($1, $2, $3, $4) RETURNING *",
+      [origen, destino, detalles, nombredestinatario]
+    );
+    res
+      .status(200)
+      .json({ message: "datos procesados", data: newData.rows[0] });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "error al cargar el archivo en el servidor", error });
+  }
+};
 
 // import fs from "fs"
 // import multer from "multer"
